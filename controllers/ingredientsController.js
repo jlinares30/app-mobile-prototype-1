9be6ff96f23d128a1f32fa6bd967ce48df868c33
jlinares <jorgelinaresrrs301@gmail.com>
2025-10-19 13:05:49 -0500

    Add getIngredientById method and update routes for ingredient retrieval

--- controllers/ingredientsController.js
+++ controllers/ingredientsController.js
@@ -9,6 +9,20 @@ export const getAllIngredients = async (req, res)=>{
     }
 }
 
+export const getIngredientById = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const ingredient = await Ingredient.findById(id);
+        if (!ingredient) {
+            return res.status(404).json({ message: 'Ingredient not found' });
+        }
+        res.status(200).json(ingredient);
+    } catch (error) {
+        res.status(500).json({ message: 'Error fetching ingredient', error });
+    }
+};
+
+
 export const createIngredient = async (req, res)=>{
     try {
         const newIngredient = new Ingredient(req.body);
--- routes/ingredientsRoutes.js
+++ routes/ingredientsRoutes.js
@@ -1,10 +1,11 @@
 import {Router} from 'express'
-import { getAllIngredients, createIngredient } from '../controllers/ingredientsController.js'
+import { getAllIngredients, createIngredient, getIngredientById } from '../controllers/ingredientsController.js'
 import authMiddleware from '../middlewares/authMiddleware.js';
 
 const router = Router();
 
-router.get('/', authMiddleware ,getAllIngredients)
-router.post('/', authMiddleware ,createIngredient)
+router.get('/', authMiddleware ,getAllIngredients);
+router.get('/:id', authMiddleware, getIngredientById);
+router.post('/', authMiddleware ,createIngredient);
 
 export default router;
